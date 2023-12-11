0840d141a389dba9e9069b76fd15433f63237324
Muhammad Naveed <naveed65dev@gmail.com>
2023-12-11 20:14:59 +0500

    update CountryModel

--- api/routes/countryRoute.js
+++ api/routes/countryRoute.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
-const authController = require('../controllers/authController');
-const emailController = require('../controllers/emailController');
-const otpController = require('../controllers/otpController');
+const countryController = require('../controllers/countryController');
+ 
 
-// Routes for userController
-router.get('/country', userController.getAllCountry);
-router.get('/country/id/:id', userController.getCountryById);
-// router.get('/users/name/:userName', userController.getUserByName);
-// router.get('/users/age/:userAge', userController.getUserByAge);
-
-// Routes for create a user
-router.post('/country', userController.createCountry);
-// Routes for delete a userz
-router.delete('/country/id/:id', userController.deletedCountryById);
-// router.delete('/users/name/:userName', userController.deletedUserByName);
-// router.delete('/users/age/:userAge', userController.deletedUserByAge);
-// Routes for update a user
-router.put('/country/id/:id', userController.updatedCountryById);
-// router.put('/users/name/:userName', userController.updatedUserByName);
-// router.put('/users/age/:userAge', userController.updatedUserByAge);
-
-
-
-
-// Routes for emailController
-//router.post('/generate-otp', emailController.generateOTP);
-
-
-// Routes for otpController
-//router.post('/reset-password', otpController.resetPassword);
-// Other OTP-related routes...
-
-// Routes for authController
-router.post('/login', authController.login);
-
-//router.get('/protected-route', authController.checkToken);
-
+// Routes for countryController
+router.get('/country', countryController.getAllCountry);
+router.get('/country/id/:id', countryController.getCountryById); 
+// Routes for create a country
+router.post('/country', countryController.createCountry);
+// Routes for delete a country
+router.delete('/country/id/:id', countryController.deletedCountryById); 
+// Routes for update a country
+router.put('/country/id/:id', countryController.updatedCountryById); 
+ 
 
 module.exports = router;
